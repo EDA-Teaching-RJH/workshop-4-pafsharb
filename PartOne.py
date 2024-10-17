151d7a8138511e81048774b79f05b65e9f5e125c
pafsharb <pa413@kent.ac.uk>
2024-10-17 13:11:35 +0000

    part 1

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,12 @@
+variable = input("Please enter a variable in camel case.")
 
+def main():
+    word = ""
+    for alphabet in variable:
+        if alphabet.isupper():
+            word += "".join("_" + alphabet.lower())
+                     
+        else:
+            word += "".join(alphabet)
+    print(word)
+main()
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,3 @@
+money = int(input("Please insert cash or change."))
 
+if money == 0.05 or money == 0.10 or money == 0.20 or money == 0.50 or money >= 1:
